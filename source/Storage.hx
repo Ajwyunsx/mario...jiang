56ed6af2602b10552de7b90c687e379a5ffa9a59
qqqeb111 <158131780+qqqeb111@users.noreply.github.com>
2024-02-03 12:31:46 +0800

    Update Storage.hx

--- source/Storage.hx
+++ source/Storage.hx
@@ -16,14 +16,32 @@ class Storage
 	public static function copyNecessaryFiles():Void
 	{
 		#if LUA_ALLOWED
-		for (dir in ['characters', 'data', 'custom_events'])
+		for (dir in ['data'])
+		{
+			for (file in Assets.list().filter(folder -> folder.startsWith('assets/$dir')))
+			{
+				if (Path.extension(file) == 'lua' || (dir == 'data' && Path.extension(file) == 'json'))
+				{
+					// Ment for FNF's libraries system...
+					final shit:String = file.replace(file.substring(0, file.indexOf('/', 0) + 1), '');
+					final library:String = shit.replace(shit.substring(shit.indexOf('/', 0), shit.length), '');
+
+					@:privateAccess
+					Storage.copyFile(Assets.libraryPaths.exists(library) ? '$library:$file' : file, file);
+				}
+			}
+		}
+		#end
+			
+		#if LUA_ALLOWED
+		for (dir in ['characters', 'custom_events'])
 		{
 			for (file in Assets.list().filter(folder -> folder.startsWith('assets/$dir')))
 			{
 				if (Path.extension(file) == 'lua' || (dir == 'custom_events' && Path.extension(file) == 'txt'))
 				{
 					// Ment for FNF's libraries system...
-					final shit:String = file.replace(file.substring(0, file.indexOf('/', 0) + 2), '');
+					final shit:String = file.replace(file.substring(0, file.indexOf('/', 0) + 1), '');
 					final library:String = shit.replace(shit.substring(shit.indexOf('/', 0), shit.length), '');
 
 					@:privateAccess
