7cd65cba54d523a03b321bdc201bfcf671e4d7cd
qqqeb111 <158131780+qqqeb111@users.noreply.github.com>
2024-02-04 15:41:57 +0800

    Update Storage.hx

--- source/Storage.hx
+++ source/Storage.hx
@@ -15,21 +15,6 @@ class Storage
 {
 	public static function copyNecessaryFiles():Void
 	{
-	        #if LUA_ALLOWED
-		for (file in Assets.list().filter(folder -> folder.startsWith('assets/characters')))
-		{
-			if (Path.extension(file) == 'lua')
-			{
-				// Ment for FNF's libraries system...
-				final shit:String = file.replace(file.substring(0, file.indexOf('/', 0) + 1), '');
-				final library:String = shit.replace(shit.substring(shit.indexOf('/', 0), shit.length), '');
-
-				@:privateAccess
-				Storage.copyFile(Assets.libraryPaths.exists(library) ? '$library:$file' : file, file);
-			}
-		}
-		#end
-		
 		#if VIDEOS_ALLOWED
 		for (file in Assets.list().filter(folder -> folder.startsWith('assets/videos')))
 		{
